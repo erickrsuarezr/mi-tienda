9882d1b16cfa01997b58f74b300500a7452d78d2
Erick Rodolfo Suarez Ruiz <erickrsuarezr@hotmail.com>
2021-10-06 13:48:59 -0500

    Creación de rutas o endpoints en texto plano o JSON

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ app.get('/', (req, res)=>{
   res.send('Hola, soy el servidor en Express');
 })
 
+app.get('/nueva-ruta', (req, res)=>{
+  res.send('Hola, soy una ruta nueva o endpoint como tambien me llaman');
+})
+
+app.get('/products', (req, res)=>{
+  res.json({
+    name: 'Producto 1',
+    price: 1300
+  });
+})
+
+app.get('/categories', (req, res)=>{
+  res.json({
+    name: 'Salud y Belleza'
+  });
+})
+
 app.listen(port, ()=>{
   console.log('Mi puerto es: '+ port);
 });
