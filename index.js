2b36969d90f6a66378454952c8b262f3a10bea2e
Erick Rodolfo Suarez Ruiz <erickrsuarezr@hotmail.com>
2021-10-06 13:40:42 -0500

    Creación de servidor web usando express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Mi aplicacion')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res)=>{
+  res.send('Hola, soy el servidor en Express');
+})
+
+app.listen(port, ()=>{
+  console.log('Mi puerto es: '+ port);
+});
