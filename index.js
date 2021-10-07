4b192a756c05b1534624243b58a812ea28b84c3b
Erick Rodolfo Suarez Ruiz <erickrsuarezr@hotmail.com>
2021-10-07 13:50:47 -0500

    Uso de parametros tipo query y evitar rutas que pueden chocar Uso de faker con npm para creación de prod

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const faker = require('faker');
+
+
 const app = express();
 const port = 3000;
 
@@ -11,25 +14,42 @@ app.get('/nueva-ruta', (req, res)=>{
 })
 
 app.get('/products', (req, res)=>{
-  res.json([
-    {
-      name: 'Product 1',
-      price: 1000
-    },
-    {
-      name: 'Product 2',
-      price: 2000
-    }
-  ]);
+  const products = [];
+  const { size }=req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(),10),
+      image: faker.image.imageUrl()
+    })
+  }
+  res.json(products);
 });
 
+app.get('/products/filter', (req, res)=>{
+  res.send('Soy un filter');
+})
+
 app.get('/products/:id', (req, res)=>{
-  const { id } = req.params;
-  res.json({
+const { id } = req.params;
+res.json({
     id,
     name: 'Product 2',
     price: 2000
   });
+})
+
+app.get('/users', (req, res)=>{
+  const { limit, offset }=req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    });
+  }else{
+    res.send('Lo siento, no hay parámetros :(')
+  }
 })
 
 app.get('/categories/:categoryId/products/:productId', (req, res)=>{
