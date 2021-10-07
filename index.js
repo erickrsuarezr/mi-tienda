ba12cba63e86a59f6da2d47ed4ec7eadefa18965
Erick Rodolfo Suarez Ruiz <erickrsuarezr@hotmail.com>
2021-10-07 14:44:37 -0500

    Se quitan las rutas para poder hacer modular la aplicacion y mas mantenible

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const faker = require('faker');
-
+const routerApi = require('./routes');
 
 const app = express();
 const port = 3000;
+
+routerApi(app);
 
 app.get('/', (req, res)=>{
   res.send('Hola, soy el servidor en Express');
@@ -13,60 +14,6 @@ app.get('/nueva-ruta', (req, res)=>{
   res.send('Hola, soy una ruta nueva o endpoint como tambien me llaman');
 })
 
-app.get('/products', (req, res)=>{
-  const products = [];
-  const { size }=req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(),10),
-      image: faker.image.imageUrl()
-    })
-  }
-  res.json(products);
-});
-
-app.get('/products/filter', (req, res)=>{
-  res.send('Soy un filter');
-})
-
-app.get('/products/:id', (req, res)=>{
-const { id } = req.params;
-res.json({
-    id,
-    name: 'Product 2',
-    price: 2000
-  });
-})
-
-app.get('/users', (req, res)=>{
-  const { limit, offset }=req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset
-    });
-  }else{
-    res.send('Lo siento, no hay parámetros :(')
-  }
-})
-
-app.get('/categories/:categoryId/products/:productId', (req, res)=>{
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-    name: 'Product 2',
-    price: 2000
-  });
-})
-
-app.get('/categories', (req, res)=>{
-  res.json({
-    name: 'Salud y Belleza'
-  });
-})
 
 app.listen(port, ()=>{
   console.log('Mi puerto es: '+ port);
