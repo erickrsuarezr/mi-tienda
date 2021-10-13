f1a3ec527835d27f54c01e65350f7db2b50e707a
Erick Rodolfo Suarez Ruiz <erickrsuarezr@hotmail.com>
2021-10-13 14:09:55 -0500

    Se agregan productos bloqueados y desbloqueados Manejo dinamico de estatus http por medio de boom Envio de los errores al middleware error

--- services/product.service.js
+++ services/product.service.js
@@ -1,4 +1,5 @@
 const faker = require('faker');
+const boom = require('@hapi/boom');
 
 class ProductsService {
 
@@ -14,7 +15,8 @@ class ProductsService {
         id: faker.datatype.uuid(),
         name: faker.commerce.productName(),
         price: parseInt(faker.commerce.price(),10),
-        image: faker.image.imageUrl()
+        image: faker.image.imageUrl(),
+        isBlock: faker.datatype.boolean(),
       })
     }
   }
@@ -38,13 +40,20 @@ class ProductsService {
 
   async findOne(id){
     // const name = this.getTotal(); //error generado para la parte uno de middlewares
-    return this.products.find(item => item.id === id);
+    const product =  this.products.find(item => item.id === id);
+    if(!product){
+      throw boom.notFound('Product not Found');
+    }
+    if(product.isBlock){
+      throw boom.conflict('The product is Blocked');
+    }
+    return product;
   }
 
   async update(id, changes){
     const index = this.products.findIndex(item => item.id === id); //Me devuelve la posicion del objeto
     if(index === -1){
-      throw new Error('Producto no encontrado');
+      throw boom.notFound('Product not Found');
     }
     const product = this.products[index];
     this.products[index]= {
@@ -57,7 +66,7 @@ class ProductsService {
   async delete(id){
     const index = this.products.findIndex(item => item.id === id); //Me devuelve la posicion del objeto
     if(index === -1){
-      throw new Error('Producto no encontrado');
+      throw boom.notFound('Product not Found');
     }
     this.products.splice(index, 1);//Splice nos permite encontrar una posicion y cuantos elementos se quieren eliminar
     return {message: 'El producto se eliminó exitosamente ', id};
