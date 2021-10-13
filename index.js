5ce548c11e099035ad1cd900983c803c0fd835b2
Erick Rodolfo Suarez Ruiz <erickrsuarezr@hotmail.com>
2021-10-13 14:07:34 -0500

    Llamado del middleware boomhandler

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routerApi = require('./routes');
-const {logErrors, errorHandler}=require('./middlewares/error.handler')
+const {logErrors, errorHandler, boomErrorHandler}=require('./middlewares/error.handler')
 
 const app = express();
 const port = 3000;
@@ -10,6 +10,7 @@ app.use(express.json());
 routerApi(app);
 
 app.use(logErrors);
+app.use(boomErrorHandler);
 app.use(errorHandler);
 
 app.get('/', (req, res)=>{
