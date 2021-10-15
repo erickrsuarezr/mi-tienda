ca26aa5a2f24ad15ef1c1aa3b6a75a2857278596
Erick Rodolfo Suarez Ruiz <erickrsuarezr@hotmail.com>
2021-10-15 14:45:09 -0500

    Se corrige el llamado a boom Se agrega la instruccion abortearly Dicha instruccion manda todos los errores al usuario

--- middlewares/validator.handler.js
+++ middlewares/validator.handler.js
@@ -1,9 +1,9 @@
-const { boom } = require('@hapi/boom');
+const boom  = require('@hapi/boom');
 
 function validatorHandler(schema, property){
   return (req, res, next) =>{
     const data = req[property];//Para obtener la información de forma dinámica
-    const {error} = schema.validate(data);
+    const { error } = schema.validate(data,{ abortEarly:false });
     if(error){
       next(boom.badRequest(error));
     }
