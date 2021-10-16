3caee055d922541685a32c145c9a6977e84ff287
Erick Rodolfo Suarez Ruiz <erickrsuarezr@hotmail.com>
2021-10-16 13:54:20 -0500

    Uso de puerto 3000 o el que mande heroku para deploy

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const routerApi = require('./routes');
 const {logErrors, errorHandler, boomErrorHandler}=require('./middlewares/error.handler')
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-const whitelist = ['http://localhost:8080', 'https://myapp.com.mx', '127,0,0,1:5500'];
+const whitelist = ['http://localhost:8080', 'https://myapp.com.mx', '127.0.0.1:5500'];
 const options = {
   origin: (origin, callback)=>{
-    if (whitelist.includes(origin)) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true);
     }else{
       callback(new Error('No permitido'));
